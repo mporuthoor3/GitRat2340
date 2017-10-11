358f36214464ab5237fd78037a373c25f6c6e854
eric8 <eric8@gatech.edu>
2017-10-11 14:19:28 -0400

    deleted two lines from main

--- app/src/main/java/a2340/m4_login/MainActivity.java
+++ app/src/main/java/a2340/m4_login/MainActivity.java
@@ -17,9 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Do not remove or run these lines
-        InputStream is = getResources().openRawResource(R.raw.rat_sightings);
-        LoadSightings.loadData(is);
         sign = (Button) findViewById(R.id.button);
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
