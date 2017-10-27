41a4ddfe5ed7dbed1e3e8881a4bc1eb630168c88
Lillie Zhou <zlillie@gatech.edu>
2017-10-26 20:03:49 -0400

    created initial UI for filtering data, pt 3

---
+++ app/src/main/java/a2340/m4_login/FilterDataActivity.java
@@ -0,0 +1,24 @@
+package a2340.m4_login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zlillie on 10/26/17.
+ */
+
+public class FilterDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filterdata);
+
+        inflateWidgets();
+    }
+
+    private void inflateWidgets() {
+
+    }
+
+}
