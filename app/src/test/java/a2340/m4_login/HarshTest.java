b68a67fe00aefa17b9512b1afc6587690318b6f5
Harsh Gupta <hg@gatech.edu>
2017-11-13 20:58:32 -0500

    Added test file

---
+++ app/src/test/java/a2340/m4_login/HarshTest.java
@@ -0,0 +1,25 @@
+package  a2340.m4_login;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import a2340.m4_login.User;
+
+public class HarshTest {
+	
+   /**
+    * Created by Harsh on 11/13/2017
+
+    * Tests Main user class instantiation and equality.
+    */
+
+    @Test
+    public void testEquals() throws Exception{
+
+        User user1 = new User("username",false,'randomid','secret');
+        User user2 = new User("username",false,'randomid','secret');
+        User user3 = new User("username1", false,"password", 'realpass');
+        assertEquals(true, user1.equals(user2));
+        assertEquals(false, user2.equals(user3));
+        assertEquals(false, user1.equals(user3));
+    }
+}
+
